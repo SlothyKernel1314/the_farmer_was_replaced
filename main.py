d89b30bbd03d5304b865c7ad55c36d2347714ce3
SlothyKernel1314 <cyrilberthout@gmail.com>
2025-06-18 20:41:48 +0200

    simple 1x3 grass or bush

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
+# CLEAR WORLD
+clear()
+
+# GRASS MODE
+# while True:
+# 	if can_harvest():
+# 		harvest()
+# 		move(North)
+
+# BUSH MODE
+# Init Field with Bush
+plant(Entities.Bush)
+move(North)
+plant(Entities.Bush)
+move(North)
+plant(Entities.Bush)
+move(North)
+# Harvest Bush
 while True:
-	harvest()
-	print("toto")
+	if can_harvest():
+		harvest()
+		plant(Entities.Bush)
+		move(North)
