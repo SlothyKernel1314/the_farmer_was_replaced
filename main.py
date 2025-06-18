71e26bf51fdf83504319d451d7a8ad7c9c3a0cd3
SlothyKernel1314 <cyrilberthout@gmail.com>
2025-06-18 21:15:22 +0200

    Carrot Mode

--- main.py
+++ main.py
@@ -10,19 +10,37 @@ clear()
 # 			move(North)
 # 		move(East)
 
-# BUSH MODE
-# Init Field with Bush
+# # BUSH MODE
+# # Init Field with Bush
+# for i in range(get_world_size()):
+# 		for j in range(get_world_size()):
+# 			plant(Entities.Bush)
+# 			move(North)
+# 		move(East)
+# # Harvest Bush
+# while True:
+# 	for i in range(get_world_size()):
+# 		for j in range(get_world_size()):
+# 			if can_harvest():
+# 				harvest()
+# 				plant(Entities.Bush)
+# 				move(North)
+# 			move(East)
+
+# CARROT MODE
+# Init Field with Carrot
 for i in range(get_world_size()):
 		for j in range(get_world_size()):
-			plant(Entities.Bush)
+			till()
+			plant(Entities.Carrot)
 			move(North)
 		move(East)
-# Harvest Bush
+# Harvest Carrot
 while True:
 	for i in range(get_world_size()):
 		for j in range(get_world_size()):
 			if can_harvest():
 				harvest()
-				plant(Entities.Bush)
+				plant(Entities.Carrot)
 				move(North)
 			move(East)
