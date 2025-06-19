879218ecf90554ee359c551e18a84e50f45f6f6f
SlothyKernel1314 <cyrilberthout@gmail.com>
2025-06-19 21:17:56 +0200

    recover bush spamming mode

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import my_utilities
 import my_init_fields
 import my_automated_fields
 
+# TODO :
+# Fertilizer
+
 # AUTOMATED PARAMETERS
 WORLD_SIZE = my_utilities.clear_and_get_world_size()
 WORLD_NUMBER_OF_TILES = WORLD_SIZE * WORLD_SIZE
@@ -9,8 +12,9 @@ WORLD_NUMBER_OF_TILES = WORLD_SIZE * WORLD_SIZE
 # MANUAL PARAMETERS
 FIELD_MODE = {
     'GRASS_SPAMMING_MODE': False,
-	'BUSH_TREE_SPAMMING_MODE': False,
+	'BUSH_SPAMMING_MODE': True,
     'CARROT_SPAMMING_MODE': False,
+	'BUSH_TREE_SPAMMING_MODE': False,
 	'PUMPKIN_SPAMMING_MODE': True
 }
 
@@ -19,6 +23,9 @@ if __name__ == "__main__":
 	if FIELD_MODE['GRASS_SPAMMING_MODE'] == True:
 		my_init_fields.init_field_with_grass(WORLD_SIZE)
 		my_automated_fields.automated_field_with_grass(WORLD_SIZE)
+	if FIELD_MODE['BUSH_SPAMMING_MODE'] == True:
+		my_init_fields.init_field_with_bush(WORLD_SIZE)
+		my_automated_fields.automated_field_with_bush(WORLD_SIZE)
 	if FIELD_MODE['BUSH_TREE_SPAMMING_MODE'] == True:
 		my_init_fields.init_field_with_bush_and_tree(WORLD_SIZE)
 		my_automated_fields.automated_field_with_bush_and_tree(WORLD_SIZE)
